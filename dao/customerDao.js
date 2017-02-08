e7e734073be699713f709bc48cf0b0489ad9946b
dongcheng li <dxl170030@utdallas.edu>
2017-02-08 16:36:57 -0600

    update profile

--- dao/customerDao.js
+++ dao/customerDao.js
@@ -21,4 +21,91 @@ CustomerDao.prototype.findOneById = function(id,callback){
         callback && callback(error, user);
     });
 };
+
+CustomerDao.prototype.update = function(id,firstName,callback){
+    var sql = "UPDATE Customer "
+            +"SET firstName=? "
+            +"WHERE id=?;";
+    var values = [firstName,id];
+    this.execute(sql, values, function(error, res){
+        callback && callback(error, res);
+    });
+}
+// CustomerDao.prototype.editOne = function( req, res){
+//     var id = req.user.id;
+
+//     console.log("edit===========>" + id);
+//     this.pool.getConnection(function(err, connection){
+
+//         var query = connection.query("SELECT * FROM customer WHERE id = ?", [id], function(err, results){
+//             if(err)
+//                 console.log("Error edit: %s", err);
+
+//             res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:results});
+//         });
+//     });
+// }
+
+// CustomerDao.prototype.updateOne = function(req, res){
+
+//     var input = JSON.parse(JSON.stringify(req.body));
+//     var id = req.params.id;
+
+//     console.log("update===========>" + id);
+
+//     this.pool.getConnection(function(err, connection){
+
+//         var data = {
+//             id    : input.id,
+//             username : input.username,
+//             firstName   : input.firstName,
+//             middleName   : input.middleName,
+//             lastName    : input.lastName,
+//             email   : input.email,
+//             telephone   : input.telephone,
+//             createdTime : input.createdTime,
+//             lastModifiedTime    : input.lastModifiedTime
+//         };
+
+//         console.log(data);
+
+//         var query = connection.query("UPDATE customer SET ? WHERE id = ?", [data, id], function(err, results){
+//             if(err)
+//             {
+//                 console.log("Error Updating: %s", err);
+//                 res.render('login');
+//             }
+
+//             var query = connection.query("SELECT * FROM customer WHERE id = ?", [id], function(err, results){
+//                 if(err)
+//                     console.log("Error edit: %s", err);
+
+//                 res.render('customer',{page_title:"Edit Customers - Node.js",data:results[0]});
+//             });
+//         });
+//     });
+// }
+
+// CustomerDao.prototype.deleteOne = function(req, res){
+//     var id = req.params.id;
+//     console.log("delete id===========>" + id);
+//     this.pool.getConnection(function(err, connection){
+
+//         connection.query("DELETE FROM customer WHERE id = ?", [id], function(err, results){
+//             if(err)
+//                 console.log("Error deleting : %s ",err );
+//             res.redirect('/login');
+//         });
+//     });
+// };
+
+
+// CustomerDao.prototype.UpdateCustomer = function(req, res){
+//     this.updateOne(req, res);
+// };
+
+// CustomerDao.prototype.DeleteCustomer = function(req, res){
+//     this.deleteOne(req, res);
+// };
+
 exports = module.exports = CustomerDao;
--- router.js
+++ router.js
@@ -2,6 +2,9 @@ var express = require('express');
 var router = express.Router();
 var auth = require('./auth');
 
+var CustomerDao = require("./dao/CustomerDao.js");
+var customer = new CustomerDao();
+
 // If the req is needed to be pre-process, do it here.
 router.use(function timeLog (req, res, next) {
   next()
@@ -31,7 +34,27 @@ router.get('/logout',auth.ensureLoggedIn(),
 
 router.get('/profile',auth.ensureLoggedIn(),
   function(req, res){
-    res.send(req.user );
+    res.render("customer",{data:req.user});
+});
+router.get('/profile/edit',auth.ensureLoggedIn(),
+  function(req, res){
+    res.render("edit_customer",{data:req.user});
+});
+
+router.post('/profile/edit',function(req,res){
+  console.log(req);
+  var id = req.body.id;
+  var username = req.body.username;
+  var firstName = req.body.firstName;
+  customer.update(id,firstName,function(error,res){
+    console.log(error,res);
+  });
+});
+
+router.delete('/profile/delete',function(req,res){
+  customer.delete(req.user.id,function(error,res){
+    console.log(error,res);
+  });
 });
 
 var AddressDao = require("./dao/AddressDao.js");
