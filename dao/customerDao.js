c07147812a5096e12a414a799f7b94f97f3375d9
ping1Z <zhongyp.design@gmail.com>
2017-02-09 15:23:19 -0600

    updates

--- dao/customerDao.js
+++ dao/customerDao.js
@@ -20,12 +20,12 @@ CustomerDao.prototype.findOneWithPassword = function(userIdentity,callback){
 // get current time method: GETDATE().
 // callback refers to the function in router.js, 
 // when the result is generated, it will callback the function in router.js.
-CustomerDao.prototype.signUp = function(email, firstName, lastName, telephone, password, callback){
+CustomerDao.prototype.signUp = function(email, firstName, lastName, password, callback){
 //    "?" represent values to be input.
 // execute(sql,values) let the values insert into the correspongding place. This is a function defined in baseDao.js.
-    var sql="INSERT INTO surprise.Customer (email, firstName, lastName, telephone, createdTime)"
-          +" VALUES ( ?, ?, ?, ?, NOW())"
-    var values=[email,firstName,lastName,telephone];
+    var sql="INSERT INTO surprise.Customer (email, firstName, lastName, createdTime)"
+          +" VALUES ( ?, ?, ?, NOW())"
+    var values=[email,firstName,lastName];
     var _ = this;
     _.execute(sql,values,function(error, res){
         console.log(1,error,res,res.affectedRows!=1);
@@ -44,96 +44,21 @@ CustomerDao.prototype.signUp = function(email, firstName, lastName, telephone, p
     });
 }
 
+CustomerDao.prototype.update = function(id, firstName, middleName, lastName, callback){
+    var sql="UPDATE Customer SET firstName=?, middleName=?, lastName=?, lastModifiedTime=NOW()"
+          +" WHERE id=?"
+    var values=[firstName, middleName, lastName,  parseInt(id)];
+    var _ = this;
+    _.execute(sql,values,function(error, res){
+        console.log(1,error,res,res.affectedRows!=1);
+        callback && callback(error, res);
+    });
+}
+
 CustomerDao.prototype.findOneById = function(id,callback){
     this.findOne(null, "`id`=\""+id+"\"",function(error, user){
         callback && callback(error, user);
     });
 };
-
-CustomerDao.prototype.update = function(id,firstName,callback){
-    var sql = "UPDATE Customer "
-            +"SET firstName=? "
-            +"WHERE id=?;";
-    var values = [firstName,id];
-    this.execute(sql, values, function(error, res){
-        callback && callback(error, res);
-    });
-}
-// CustomerDao.prototype.editOne = function( req, res){
-//     var id = req.user.id;
-
-//     console.log("edit===========>" + id);
-//     this.pool.getConnection(function(err, connection){
-
-//         var query = connection.query("SELECT * FROM customer WHERE id = ?", [id], function(err, results){
-//             if(err)
-//                 console.log("Error edit: %s", err);
-
-//             res.render('edit_customer',{page_title:"Edit Customers - Node.js",data:results});
-//         });
-//     });
-// }
-
-// CustomerDao.prototype.updateOne = function(req, res){
-
-//     var input = JSON.parse(JSON.stringify(req.body));
-//     var id = req.params.id;
-
-//     console.log("update===========>" + id);
-
-//     this.pool.getConnection(function(err, connection){
-
-//         var data = {
-//             id    : input.id,
-//             username : input.username,
-//             firstName   : input.firstName,
-//             middleName   : input.middleName,
-//             lastName    : input.lastName,
-//             email   : input.email,
-//             telephone   : input.telephone,
-//             createdTime : input.createdTime,
-//             lastModifiedTime    : input.lastModifiedTime
-//         };
-
-//         console.log(data);
-
-//         var query = connection.query("UPDATE customer SET ? WHERE id = ?", [data, id], function(err, results){
-//             if(err)
-//             {
-//                 console.log("Error Updating: %s", err);
-//                 res.render('login');
-//             }
-
-//             var query = connection.query("SELECT * FROM customer WHERE id = ?", [id], function(err, results){
-//                 if(err)
-//                     console.log("Error edit: %s", err);
-
-//                 res.render('customer',{page_title:"Edit Customers - Node.js",data:results[0]});
-//             });
-//         });
-//     });
-// }
-
-// CustomerDao.prototype.deleteOne = function(req, res){
-//     var id = req.params.id;
-//     console.log("delete id===========>" + id);
-//     this.pool.getConnection(function(err, connection){
-
-//         connection.query("DELETE FROM customer WHERE id = ?", [id], function(err, results){
-//             if(err)
-//                 console.log("Error deleting : %s ",err );
-//             res.redirect('/login');
-//         });
-//     });
-// };
-
-
-// CustomerDao.prototype.UpdateCustomer = function(req, res){
-//     this.updateOne(req, res);
-// };
-
-// CustomerDao.prototype.DeleteCustomer = function(req, res){
-//     this.deleteOne(req, res);
-// };
 
 exports = module.exports = CustomerDao;
--- router.js
+++ router.js
@@ -3,19 +3,14 @@ var express = require('express');
 var router = express.Router();
 var auth = require('./auth');
 var CustomerDao = require("./dao/customerDao.js");
-var UserDao = require("./dao/userDao.js");
-
-// create a new instance.
 var customer = new CustomerDao();
-var user = new UserDao();
-
-var CustomerDao = require("./dao/CustomerDao.js");
-var customer = new CustomerDao();
+var AddressDao = require("./dao/addressDao.js");
+var address = new AddressDao();
 
 // If the req is needed to be pre-process, do it here.
 router.use(function timeLog (req, res, next) {
   next()
-})
+});
 
 router.get("/",
     function(req,res){
@@ -30,13 +25,12 @@ router.post('/signUp',
      var email = req.body.email;
      var firstName = req.body.firstName;
      var lastName = req.body.lastName;
-     var telephone = req.body.telephone;
      var password = req.body.password;
      var confirmPassword = req.body.confirmPassword;
 
     // call signUp method and send the parameter values to the method.
     // res refers to the result from database.
-    customer.signUp(email, firstName, lastName, telephone, password, function(e, r){
+    customer.signUp(email, firstName, lastName, password, function(e, r){
       res.render('login');    
     });
   });
@@ -70,29 +64,89 @@ router.get('/profile',auth.ensureLoggedIn(),
     res.render("profile",{profile:req.user});
 });
 
-router.get('/address',auth.ensureLoggedIn(),
+router.post('/updateProfile',auth.ensureLoggedIn(),
   function(req, res){
-    res.render("address",{profile:req.user});
+     var id = req.body.id;
+     if(id!=req.user.id){
+       res.redirect('profile');
+     }
+     var firstName = req.body.firstName;
+     var middleName = req.body.middleName;
+     var lastName = req.body.lastName;
+     customer.update(id, firstName, middleName, lastName, function(e, r){
+      res.redirect('profile');
+    });
 });
+
+router.get('/listAddress',auth.ensureLoggedIn(),
+  function(req, res){
+    address.findByCustomerId(req.user.id,function(err,address){
+      res.render("addressList",{addressList:address});
+    });
+    
+});
+
+router.get('/addAddress',auth.ensureLoggedIn(),
+  function(req, res){
+    res.render("address",{address:{id:"add",customerId:req.user.id}});
+});
+
+router.get('/editAddress',auth.ensureLoggedIn(),
+  function(req, res){
+    var id = parseInt(req.query.id);
+    var customerId = req.user.id;
+    address.findById(id,customerId,function(e,address){
+        res.render("address",{address:address});
+    });
+});
+
+router.post('/saveAddress',auth.ensureLoggedIn(),
+  function(req, res){
+    var customerId = parseInt(req.body.customerId);
+    if(customerId!=req.user.id){
+      res.redirect('listAddress');
+    }
+    var id = req.body.id;
+    var name = req.body.name;
+    var line1 = req.body.line1;
+    var line2 = req.body.line2;
+    var city = req.body.city;
+    var state = req.body.state;
+    var country = req.body.country;
+    var zipcode = req.body.zipcode;
+    if(!id||id=="add"){
+       address.addAddress(customerId,name,line1,line2,city,state,country,zipcode,function(e,r){
+          res.redirect('listAddress');
+      });
+    }else{
+       address.updateAddress(id,name,line1,line2,city,state,country,zipcode,function(e,r){
+          res.redirect('listAddress');
+      });
+    }
+});
+router.get('/setAsDefaultAddress',auth.ensureLoggedIn(),
+  function(req, res){
+    var id = parseInt(req.query.id);
+    var customerId = req.user.id;
+    address.setAsDefault(id,customerId,function(e,r){
+          res.redirect('listAddress');
+    });
+});
+router.get('/deleteAddress',auth.ensureLoggedIn(),
+  function(req, res){
+    var id = parseInt(req.query.id);
+    var customerId = req.user.id;
+    address.deleteAddress(id,customerId,function(e,r){
+          res.redirect('listAddress');
+    });
+
+});
+
 
 router.get('/payment',auth.ensureLoggedIn(),
   function(req, res){
     res.render("payment",{profile:req.user});
 });
 
-router.get('/listAddress',auth.ensureLoggedIn(),
-  function(req, res){
-    Address.findByCustomerId(1,function(err,address){
-      res.send(address);
-    });
-});
-
-router.get('/addAddress',auth.ensureLoggedIn(),
-  function(req, res){    
-});
-
-router.get('/deleteAddress',auth.ensureLoggedIn(),
-  function(req, res){
-});
 
 module.exports = router;
