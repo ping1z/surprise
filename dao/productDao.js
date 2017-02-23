66dd25abc87c378b3f955b6a0c27334b59a87adb
Yihe <xyhfox741@gmail.com>
2017-02-22 18:55:02 -0600

    added single product page need to setup connection with search result next step: add add-to-shopping-cart function

---
+++ dao/productDao.js
@@ -0,0 +1,18 @@
+var BaseDao = require("./baseDao.js");
+
+var ProductDao = function(){
+    this.table = 'Product';
+}
+
+ProductDao.prototype = Object.create(BaseDao.prototype);
+ProductDao.prototype.findByProductSKU = function(productSKU,callback){
+    this.findOne(null, "`productSKU`=\""+productSKU+"\"",function(error, user){
+        callback && callback(error, user);
+    });
+};
+
+ProductDao.prototype.addToCart = function(productSKU,callback){
+    this.findOne(null, "`productSKU`=\""+productSKU+"\"",function(error, user){
+        callback && callback(error, user);
+    });
+};
